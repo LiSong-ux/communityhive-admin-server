b0cf5b1b17a722aa057e207cff4b1d89a67ea0c2
未央 <hiveforwork@163.com>
2019-12-23 12:48:56 +0800

    新增持久层

---
+++ src/main/java/net/industryhive/dao/AccessMapper.java
@@ -0,0 +1,31 @@
+package net.industryhive.dao;
+
+import net.industryhive.bean.Access;
+import net.industryhive.bean.AccessExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AccessMapper {
+    long countByExample(AccessExample example);
+
+    int deleteByExample(AccessExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Access record);
+
+    int insertSelective(Access record);
+
+    List<Access> selectByExample(AccessExample example);
+
+    Access selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Access record, @Param("example") AccessExample example);
+
+    int updateByExample(@Param("record") Access record, @Param("example") AccessExample example);
+
+    int updateByPrimaryKeySelective(Access record);
+
+    int updateByPrimaryKey(Access record);
+}
