b64da506b955632b88e8778a1b25a5706868ed04
李嵩 <lisong@pansoft.com>
2019-12-23 16:33:31 +0800

    新增管理员登录功能，未完成

---
+++ src/main/java/net/industryhive/controller/UserController.java
@@ -0,0 +1,80 @@
+package net.industryhive.controller;
+
+import net.industryhive.bean.Login;
+import net.industryhive.bean.User;
+import net.industryhive.entity.UnifiedResult;
+import net.industryhive.service.LoginService;
+import net.industryhive.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 用户管理Controller
+ *
+ * @author 未央
+ * @create 2019-12-23 15:52
+ */
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private LoginService loginService;
+
+    public UnifiedResult login(HttpServletRequest request){
+        HttpSession session = request.getSession();
+
+        //先判断用户是否已经登录
+        if (session.getAttribute("user") != null) {
+            return UnifiedResult.build(400, "用户已登录", null);
+        }
+
+        String account = request.getParameter("account");
+        String password = request.getParameter("password");
+
+        User user = userService.getUserByAccount(account);
+        if (user == null || !user.getPassword().equals(password)) {
+            return UnifiedResult.build(400, "账号或密码错误", null);
+        }
+
+        session.setAttribute("user", user);
+
+        // 用户登录次数+1
+        user.setLogincount(user.getLogincount() + 1);
+        userService.updateUser(user);
+
+        //记录用户登录ip、时间
+        Login newLogin = new Login();
+        newLogin.setIp(request.getRemoteAddr());
+        newLogin.setUserId(user.getId());
+        newLogin.setTime(new Date());
+        //记录用户登录终端
+        String terminal = request.getParameter("terminal");
+        newLogin.setTerminal(terminal);
+
+        loginService.addLogin(newLogin);
+
+        return UnifiedResult.ok(user);
+    }
+
+    @RequestMapping("/allUser")
+    @ResponseBody
+    public UnifiedResult getAllUser(HttpServletRequest request,Integer page) {
+        User admin = (User) request.getSession().getAttribute("admin");
+        if (admin==null){
+            return UnifiedResult.build(400,"您还未登录，请先登录",null);
+        }
+        List<User> userList = userService.getAllUser(page);
+        return UnifiedResult.ok(userList);
+    }
+
+}
--- src/main/java/net/industryhive/service/UserService.java
+++ src/main/java/net/industryhive/service/UserService.java
@@ -1,9 +1,60 @@
 package net.industryhive.service;
 
+import net.industryhive.bean.User;
+import net.industryhive.bean.UserExample;
+import net.industryhive.dao.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * 用户管理Service
+ *
  * @author 未央
  * @create 2019-12-23 10:06
  */
+@Service
 public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 获取全部用户信息
+     * <p>
+     * id,account,username,email,gender,
+     * topicCount,replyCount,loginCount,registerTime
+     *
+     * @param page
+     * @return
+     */
+    public List<User> getAllUser(int page) {
+        int startRow = (page - 1) * 100;
+        List<User> userList = userMapper.selectAllUser(startRow);
+        return userList;
+    }
+
+    /**
+     * 根据用户账号获取用户信息
+     *
+     * @param account
+     * @return
+     */
+    public User getUserByAccount(String account) {
+        UserExample example = new UserExample();
+        UserExample.Criteria criteria = example.createCriteria();
+        criteria.andAccountEqualTo(account);
+        List<User> userList = userMapper.selectByExample(example);
+        if (userList.isEmpty()) {
+            return null;
+        }
+        User user = userList.get(0);
+        return user;
+    }
+
+    public void updateUser(User updatedUser) {
+        userMapper.updateByPrimaryKeySelective(updatedUser);
+    }
+
 }
