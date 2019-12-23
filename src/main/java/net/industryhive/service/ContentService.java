ef6a1d30f031d596b229bb5720ced0a621533f5e
李嵩 <lisong@pansoft.com>
2019-12-23 10:13:38 +0800

    新增UnifiedResult、ContentService、UserService

---
+++ src/main/java/net/industryhive/service/ContentService.java
@@ -0,0 +1,9 @@
+package net.industryhive.service;
+
+/**
+ * 内容管理Service
+ * @author 未央
+ * @create 2019-12-23 10:07
+ */
+public class ContentService {
+}
---
+++ src/main/java/net/industryhive/service/UserService.java
@@ -0,0 +1,9 @@
+package net.industryhive.service;
+
+/**
+ * 用户管理Service
+ * @author 未央
+ * @create 2019-12-23 10:06
+ */
+public class UserService {
+}
